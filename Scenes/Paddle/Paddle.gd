d101a70e3dfa370849e3def9ec0469e6c94f7366
Filipe Augusto <filipecordeiro213@gmail.com>
2025-07-20 10:05:57 -0300

    Paddle Input

--- Scenes/Paddle/Paddle.gd
+++ Scenes/Paddle/Paddle.gd
@@ -1,7 +1,15 @@
 extends Area2D
 
+const SPEED: int = 200
+
 func _ready() -> void:
 	pass # Replace with function body.
 
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	#faz a nuvem se mover para a esquerda ao apertar A
+	if Input.is_action_pressed('left') == true:
+		position.x -= SPEED * delta
+	
+	#faz a nuvem se mover para a direta ao apertar D
+	elif Input.is_action_pressed('right') == true:
+		position.x += SPEED * delta
