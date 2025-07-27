6b15076ef3a882e7db7a69ae4c4afed3fad6374e
Filipe Augusto <filipecordeiro213@gmail.com>
2025-07-27 10:55:44 -0300

    Delimitando o paddle em um max e min da tela utilizando o metodo clampf, além disso, mudamos a forma como movimentamos o paddle utilizando o metodo Input.get_axis

--- Scenes/Paddle/Paddle.gd
+++ Scenes/Paddle/Paddle.gd
@@ -6,10 +6,14 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
-	#faz a nuvem se mover para a esquerda ao apertar A
-	if Input.is_action_pressed('left') == true:
-		position.x -= SPEED * delta
 	
-	#faz a nuvem se mover para a direta ao apertar D
-	elif Input.is_action_pressed('right') == true:
-		position.x += SPEED * delta
+	#Outra forma com menos codigo para mover o paddle
+	var movement: float = Input.get_axis('left', 'right')
+	position.x += SPEED * delta * movement
+	
+	#delimita a area que o paddle pode se mover
+	position.x = clampf(
+		position.x,
+		get_viewport_rect().position.x,
+		get_viewport_rect().end.x
+	)
